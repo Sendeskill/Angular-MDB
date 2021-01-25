fa2dcd8d7581b5ed4488c1e403c8b1ba04049d00
Lucas Tondo Sendeski <lucastsendeski@hotmail.com>
2021-01-25 18:11:56 -0300

    Melhoria - Adicionado o home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { HomeComponent } from './layouts/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  // {path: '', redirectTo: 'login', pathMatch: 'full', component: AppComponent},
+  { path: 'home', component: HomeComponent },
   // 404 - {path: '**', component: HomeComponent},
 ];
 
